3fcd3c80222f47f034d3d398600888a704feb87f
FagnerRumenigg <rumeniggmoraes@gmail.com>
2024-02-12 21:20:50 -0300

    Adicionando gráficos

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-welcome',
@@ -8,10 +10,74 @@ import { NzTabsModule } from 'ng-zorro-antd/tabs';
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('pieChart') pieChart!: ElementRef;
+  @ViewChild('lineChart') lineChart!: ElementRef;
+  @ViewChild('barChart') barChart!: ElementRef;
+
 
   constructor() { }
 
   ngOnInit() { }
 
+  ngAfterViewInit(): void {
+    const pieCtx = this.pieChart.nativeElement.getContext('2d');
+    new Chart(pieCtx, {
+      type: 'pie',
+      data: {
+        labels: ['Red', 'Green', 'Blue'],
+        datasets: [{
+          label: 'My Pie Chart',
+          data: [300, 200, 100],
+          backgroundColor: ['#ff6384', '#36a2eb', '#ffce56']
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    });
+
+    const lineCtx = this.lineChart.nativeElement.getContext('2d');
+    new Chart(lineCtx, {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+          label: 'Gráfico de linha',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    });
+
+    const ctx = this.barChart.nativeElement.getContext('2d');
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+          label: 'Gráfico de barra',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 }
